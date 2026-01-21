0ef9b3d51e1b16108bb2675a306a275fa18891d1
Nicolau45 <emamota98@gmail.com>
2026-01-21 22:56:01 +0000

    Exercicio 08 acabado

--- C2_CFundamentals/README.md
+++ C2_CFundamentals/README.md
@@ -47,4 +47,5 @@ write a program that calculates the remaining balance on a loan after the first,
 >Balance remaining after first payment: $19713.34
 >Balance remaining after second payment: $19425.25
 >Balance remaining after third payment: $19135.71
+
 Display each balance with two digits after the decimal point. Hint: Each month, the balance is decreased by the amount of the payment, but increased by the balance imes the monthly interest rate. To find the monthly interest rate, convert the interest rate entered by the user to a percentage and divide it by 12.
--- C2_CFundamentals/ex08.c
+++ C2_CFundamentals/ex08.c
@@ -1,10 +1,29 @@
 //ex08.c
+//Importante referir que os valores n estão a dar o mesmo que o exemplo pois
+//Acredito que haja algo com os floats e arredondamentos
 
 #include <stdio.h>
 
 int main(void){
 
-    
+    float loan=20000.00, interest=6.0, monthlyPay=386.66, balance, monthlyInterest;
+
+    monthlyInterest = (interest * 0.01) / 12;
+
+    balance = loan - monthlyPay;
+    float temp = balance * monthlyInterest;
+    balance += temp;
+    printf("Balance remaining after first payment: $%.2f\n", balance);
+
+    balance -= monthlyPay;
+    temp = balance * monthlyInterest;
+    balance += temp;
+    printf("Balance remaining after second payment: $%.2f\n", balance);
+
+    balance -= monthlyPay;
+    temp = balance * monthlyInterest;
+    balance += temp;
+    printf("Balance remaining after third payment: $%.2f\n", balance);
 
     return 0;
 }
